256f97e35bf68ff006ca31da99e7e3169b81fc85
Meghna Gite <meghnagite@gmail.com>
2025-01-22 21:42:36 -0500

    display image, press button

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QHBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap
+# need to add pyqt5 to prerequisites
+
+def on_click():
+    print("button pressed")
+
 def main() -> None:
     """Program entrypoint"""
-    return
+
+    app = QApplication([])
+    window = QWidget()
+    label = QLabel("YEAHHHHHHH")
+    pixmap = QPixmap("yeah.jpg")
+    button = QPushButton("send message", window)
+    button.clicked.connect(on_click)
+    label.setPixmap(pixmap)
+
+    layout = QHBoxLayout(window)
+    layout.addWidget(label)
+    layout.addWidget(button)
+
+    window.show()
+    app.exec()
+    
 
 
 if __name__ == "__main__":
