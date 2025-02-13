e5f5302b1ca36eefe0a3abba9df3b4512231e542
Meghna Gite <meghnagite@gmail.com>
2025-02-13 00:19:30 -0500

    get image data from server

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QWidget
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 import sys
 # need to add pyqt5 to prerequisites
@@ -8,10 +8,11 @@ import sys
 
 import time
 from queue import Queue, Empty
+import cv2
 
 from legolas_common.src.packet_types import BROADCAST_DEST, Packet, PacketType
 from legolas_common.src.socket_client import SocketClient
-#TODO: change legolas_common/src/socket_client to not consume packets from incoming queue
+#TODO: ensure legolas_common/src/socket_client does not consume packets from incoming queue
 
 
 class ReadIncomingMsgThread(QThread):
@@ -25,14 +26,22 @@ class ReadIncomingMsgThread(QThread):
 
     def run(self):
         while True:
-            time.sleep(1)
+            # time.sleep(1)
             try:
                 new_packet = self.incoming_data.get_nowait() # need to make sure socket_client isn't eating these messages first
                 print("RECEIVED MESSAGE FROM SERVER", GUI.packet_to_str(new_packet))
+                print("LENGTH OF INCOMING QUEUE", self.incoming_data.qsize())
+                if new_packet.packet_type == PacketType.CONTROL:
+                    print("TIME DIFFERENTIAL", time.time() - new_packet.payload["time"])
+                if new_packet.packet_type == PacketType.IMAGE:
+                    self.change_picture.emit(new_packet)
+                else:
+                    self.change_incoming_message.emit(new_packet)
             except Empty:
                 # print("no msg from server")
                 continue
-            self.change_incoming_message.emit(new_packet)
+            
+           
 
 
 
@@ -47,8 +56,8 @@ class GUI(QWidget):
         pixmap = QPixmap("yeah.jpg")
         self.button = QPushButton("send message", self)
         self.button.clicked.connect(self.on_click)
-        self.change_img_button = QPushButton("change image", self)
-        self.change_img_button.clicked.connect(self.change_img)
+        # self.change_img_button = QPushButton("change image", self)
+        # self.change_img_button.clicked.connect(self.change_img)
         self.original_img = True
 
         self.label_img.setPixmap(pixmap)
@@ -57,12 +66,13 @@ class GUI(QWidget):
         layout.addWidget(self.incoming_msg_label)
         layout.addWidget(self.label_img)
         layout.addWidget(self.button)
-        layout.addWidget(self.change_img_button)
+        # layout.addWidget(self.change_img_button)
     
         self.setLayout(layout)
 
         self.update_thread = ReadIncomingMsgThread(incoming_data)
         self.update_thread.change_incoming_message.connect(self.update_incoming_msg)
+        self.update_thread.change_picture.connect(self.change_img)
         self.incoming_data = incoming_data
         self.outgoing_data = outgoing_data
         self.update_thread.start()
@@ -76,18 +86,24 @@ class GUI(QWidget):
                         {"hello": "server", "time": time.time()},
                     ))
 
-    def change_img(self):
-        if self.original_img: 
-            new_pixmap = QPixmap("yeah2.jpg")
-            self.label_img.setPixmap(new_pixmap)
-            self.original_img = False
-        else:
-            old_pixmap = QPixmap("yeah.jpg")
-            self.label_img.setPixmap(old_pixmap)
-            self.original_img = True
+    @pyqtSlot(Packet)
+    def change_img(self, msg: Packet):
+
+        """Updates the image_label with a new opencv image"""
+        qt_img = self.convert_cv_qt(msg.payload)
+        self.label_img.setPixmap(qt_img)
+    
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        # p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(convert_to_Qt_format)
 
     @pyqtSlot(Packet)
-    def update_incoming_msg(self, msg:Packet):
+    def update_incoming_msg(self, msg: Packet):
         text = GUI.packet_to_str(msg)
         self.incoming_msg_label.setText(text)
 
